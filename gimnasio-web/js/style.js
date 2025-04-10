e6e5467273e0e50cfe3cd2202f7b7d8111a81472
beto_lage16@hotmail.com <beto_lage16@hotmail.com>
2025-04-09 22:12:21 -0300

    Primer commit: proyecto Gladiador Gym

---
+++ gimnasio-web/js/style.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById('modo-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  toggleBtn.textContent = document.body.classList.contains('dark-mode')
+    ? 'DARK MODE OFF ☀️'
+    : 'DARK MODE ON 🌙';
+});
